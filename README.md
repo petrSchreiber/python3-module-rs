55a8733018486b881e1ea57097fcaff81208a37a
petrSchreiber <petrschreiber@gmail.com>
2019-03-31 20:14:10 +0200

    feat: barebones example

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ The code is designed to run with Rust from stable channel.
 Before building:
 - ensure you have Python 3.3 or newer installed on build machine
 - ensure PATH to contain the path to `python.exe`
+
+Build the library by `cargo build --release`.
+
+The binary can be found in `target/release` afterwards:
+- on Windows, change the binary extension from `.dll` to `.pyd`
+- on Mac, change the binary extension from `.dylib` to `.so`
+
+## How to try
+Copy the binary with appropriate extension to the same directory as your script or along your Python binary.
+
+Then, you can do:
+```
+import python3_module_rs
+
+print(python3_module_rs.__doc__)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use] extern crate cpython;  // Needed for py_module_initializer! macro
+
+// Module initialization, details explained at https://dgrunwald.github.io/rust-cpython/doc/cpython/macro.py_module_initializer.html
+py_module_initializer!(python3_module_rs,                   // The module name as a Rust identifier
+                       initpython3_module_rs,               // The same as first parameter, but with init prefix
+                       PyInit_python3_module_rs, |py, m| {  // The same as first parameter, but with PyInit_ prefix
+    m.add(py, "__doc__", "Python 3 module example")?;
+
+    Ok(())
+});
